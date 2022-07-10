c5abed3bb74de5186aceb59c2dea48c0935ce804
Francesco Gaudio <francescogaudio003@gmail.com>
2022-07-10 20:49:40 +0200

    Fixing the details

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DietPlan from './DietPlan.js';
 import DietPlanSettings from './DietPlanSettings';
-import GymPlanning from './GymPlanning';
 
 //APP.JS HANDLES THE NAVIGATION
 const Stack = createNativeStackNavigator();
@@ -14,7 +13,6 @@ export default function App(props)
       <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='DietPlan'>
         <Stack.Screen name="DietPlan" component={DietPlan} />
         <Stack.Screen name="DietPlanSettings" component={DietPlanSettings} />
-        <Stack.Screen name="GymPlanning" component={GymPlanning} />
       </Stack.Navigator>
     </NavigationContainer>
   );
